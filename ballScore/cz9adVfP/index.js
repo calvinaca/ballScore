3ececf9ab4020731a3a8f8ee8a8fcae3bce3a893
calvinaca <calvinagaba60@gmail.com>
2022-10-18 16:02:54 +0530

    increments

--- ballScore/cz9adVfP/index.js
+++ ballScore/cz9adVfP/index.js
@@ -1,13 +1,31 @@
 let homeScore = 0;
 let guestScore = 0;
 
-let homeEl = document.getElementById("home-scs").textContent= homeScore;
-let guestEl = document.getElementById("guest-scs").textContent = guestScore;
+let homeEl = document.getElementById("home-scs");
+let guestEl = document.getElementById("guest-scs");
 
-function plus1() {
- homeScore += 1;
-  homeEl = homeScore;
+function homePlus1() {
+  homeScore += 1;
+  homeEl.textContent = homeScore;
+}
+function homePlus2() {
+  homeScore += 2;
+  homeEl.textContent = homeScore;
+}
+function homePlus3() {
+  homeScore += 3;
+  homeEl.textContent = homeScore;
+}
+function guestPlus1() {
+  guestScore += 1;
+  guestEl.textContent = guestScore;
+}
+function guestPlus2() {
+  guestScore += 2;
+  guestEl.textContent = guestScore;
+}
+function guestPlus3() {
+  guestScore += 3;
+  guestEl.textContent = guestScore;
 }
 
-// console.log(homeEl);
-// console.log(guestEl);
